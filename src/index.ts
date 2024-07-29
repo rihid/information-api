4b015be42d55593ebd77ac1cbc575a897bbbd910
rihid <rizalhidayat006@gmail.com>
2024-07-29 22:01:12 +0700

    feat: implement REST API

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,20 @@ app.get('/', (c) => {
     message: "Hello",
   })
 })
+app.get('/items', (c) => {
+  return c.json({
+    ok: true,
+    message: "Item"
+  })
+})
+app.get('/items/:id', (c) => {
+  const id = c.req.param('id')
+  return c.json({
+    ok: true,
+    param: id,
+    message: 'Items Detail!',
+  })
+})
 
 const port = 3000
 console.log(`Server is running on port ${port}: http://localhost:3000/`)
